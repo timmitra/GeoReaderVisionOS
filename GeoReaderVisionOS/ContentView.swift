0a4b88480f4fc10ac16a737d65f88cf0aa910da1
Tim Mitra <tim@it-guy.com>
2024-01-29 10:32:59 -0500

    setting up test

--- GeoReaderVisionOS/ContentView.swift
+++ GeoReaderVisionOS/ContentView.swift
@@ -10,15 +10,21 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    
+    GeometryReader { geometryProxy in
+      VStack(spacing: 10) {
+        Text("Width: \(geometryProxy.size.width)")
+          .position(x: geometryProxy.size.width/12,
+                    y: geometryProxy.size.height/30)
+        Text("Height: \(geometryProxy.size.height)")
+          .position(x: geometryProxy.size.width / 1.1,
+                    y: geometryProxy.size.height / 2.5 )
+      }
+      .padding()
     }
+    .background(Color.pink)
+  }
 }
 
 #Preview(windowStyle: .automatic) {
