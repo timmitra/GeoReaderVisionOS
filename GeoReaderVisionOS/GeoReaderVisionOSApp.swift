207492f2f4d879054cfa7ff644c76642ee1945d3
Tim Mitra <tim@it-guy.com>
2024-01-28 23:59:59 -0500

    Initial Commit

---
+++ GeoReaderVisionOS/GeoReaderVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoReaderVisionOSApp.swift
+//  GeoReaderVisionOS
+//
+//  Created by Tim Mitra on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct GeoReaderVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
